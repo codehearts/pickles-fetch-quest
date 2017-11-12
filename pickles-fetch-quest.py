5234657abc8cfb59450363795b18ace3be3620eb
Kate Hart <codehearts@users.noreply.github.com>
2017-11-11 16:57:15 -0800

    Load test tile

--- pickles-fetch-quest.py
+++ pickles-fetch-quest.py
@@ -1,7 +1,12 @@
 from graphics_controller import GraphicsController
+from disk_loader import DiskLoader
 import pyglet.app
 
+DiskLoader.set_resource_paths(['resources/'])
+
 pickle_graphics = GraphicsController(160, 140, title="Pickle's Fetch Quest")
+
+tile_image = DiskLoader.load_image('tiles/test.gif')
 
 if __name__ == "__main__":
     pyglet.app.run()
