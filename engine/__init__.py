582c995164c8e9833bc467e15fcccca5fbbc7809
Kate Hart <codehearts@users.noreply.github.com>
2017-11-20 13:35:23 -0800

    Add physics demo

--- engine/__init__.py
+++ engine/__init__.py
@@ -2,6 +2,7 @@ from .graphics_controller import GraphicsController, GraphicsObject
 from .disk_loader import DiskLoader
 from .geometry import Rectangle
 from .game_objects import Tile
+from .physics import Physics2d
 
-__all__ = ['DiskLoader', 'GraphicsController', 'GraphicsObject', 'Rectangle',
-           'Tile']
+__all__ = ['DiskLoader', 'GraphicsController', 'GraphicsObject', 'Physics2d',
+           'Rectangle', 'Tile']
