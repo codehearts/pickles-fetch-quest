dc2cd61a6cbf602465de07a19f120202a52103df
Kate Hart <codehearts@users.noreply.github.com>
2017-11-19 19:33:53 -0800

    Refactor Rectangle to use Point2d

--- engine/geometry/rectangle.py
+++ engine/geometry/rectangle.py
@@ -1,12 +1,15 @@
+from .point_2d import Point2d
+
+
 class Rectangle(object):
     """Two-dimensional rectangle anchored to its lower left corner.
 
     Attributes:
         x (int): The x coordinate of the left edge of the rectangle.
         y (int): The y coordinate of the bottom edge of the rectangle.
+        coordinates (:obj:`Point2d`): The coordinates as a :obj:`Point2d`.
         width (int): The width of the rectangle.
         height (int): The height of the rectangle.
-        position (tuple of int): The (x, y) coordinates of the rectangle.
     """
 
     def __init__(self, x, y, width, height):
@@ -19,12 +22,16 @@ class Rectangle(object):
             height (int): The height of the rectangle.
         """
         super(Rectangle, self).__init__()
-        self.x = x
-        self.y = y
+        self.coordinates = Point2d(x, y)
         self.width = width
         self.height = height
 
     @property
-    def position(self):
-        """Returns the x, y coordinates as a tuple."""
-        return (self.x, self.y)
+    def x(self):
+        """Returns the x coordinate of rectangle."""
+        return self.coordinates.x
+
+    @property
+    def y(self):
+        """Returns the y coordinate of rectangle."""
+        return self.coordinates.y
