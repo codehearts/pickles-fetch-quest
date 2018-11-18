c4d6a22fa9b8ec887504ef1fc3713949a443ecf6
Kate Hart <codehearts@users.noreply.github.com>
2018-11-17 22:43:34 -0800

    Add deceleration to physics simulation

--- engine/physics/tests/test_two_dimensional_physics.py
+++ engine/physics/tests/test_two_dimensional_physics.py
@@ -17,23 +17,27 @@ class TestPhysics2d(unittest.TestCase):
 
     def test_create_2d_physics(self):
         """Creating 2d physics sets initial velocity and acceleration."""
-        physics = Physics2d(mass=1, velocity=(1, 2), acceleration=(3, 4),
-                            gravity=(5, 6), terminal_velocity=(7, 8))
+        physics = Physics2d(mass=1, gravity=(5, 6), terminal_velocity=(7, 8))
+        physics.velocity.set((1, 2))
+        physics.acceleration.set((3, 4))
+
         self.assertPoint((1, 2), physics.velocity)
         self.assertPoint((3, 4), physics.acceleration)
 
     def test_simulate_with_mass(self):
         """Mass increases acceleration."""
-        physics = Physics2d(mass=2, velocity=(0, 0), acceleration=(1, 1),
-                            gravity=(1, 1), terminal_velocity=(100, 100))
+        physics = Physics2d(mass=2, gravity=(1, 1), terminal_velocity=(10, 10))
+        physics.acceleration.set((1, 1))
+
         # Mass of 2 doubles external and gravitational acceleration (1+1)*2
         physics.run_simulation(1000)
         self.assertPoint((4, 4), physics.velocity)
 
     def test_assign_velocity(self):
         """Assigning velocity changes velocity accordingly."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(2, 2),
-                            gravity=(3, 3), terminal_velocity=(100, 100))
+        physics = Physics2d(mass=1, gravity=(3, 3), terminal_velocity=(50, 50))
+        physics.acceleration.set((2, 2))
+
         physics.run_simulation(500)  # (0 + 2.5 = 2.5, 0 + 2.5 = 2.5)
         self.assertPoint((2, 2), physics.velocity)  # 2 from int rounding
 
@@ -47,8 +51,9 @@ class TestPhysics2d(unittest.TestCase):
 
     def test_assign_acceleration(self):
         """Assigning acceleration changes velocity accordingly."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(2, 2),
-                            gravity=(3, 3), terminal_velocity=(100, 100))
+        physics = Physics2d(mass=1, gravity=(3, 3), terminal_velocity=(50, 50))
+        physics.acceleration.set((2, 2))
+
         physics.run_simulation(500)  # (0 + 2.5 = 2.5, 0 + 2.5 = 2.5)
         self.assertPoint((2, 2), physics.velocity)  # 2 from int rounding
 
@@ -64,37 +69,40 @@ class TestPhysics2d(unittest.TestCase):
 
     def test_simulate_positive_x(self):
         """Positive x acceleration increases velocity."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(2, 0),
-                            gravity=(3, 0), terminal_velocity=(100, 100))
+        physics = Physics2d(mass=1, gravity=(3, 0), terminal_velocity=(5, 5))
+        physics.acceleration.set((2, 0))
+
         physics.run_simulation(500)
-        self.assertPoint((2, 0), physics.velocity)  # 2 from int rounding
+        self.assertPoint((2, 0), physics.velocity)  # 2 from int truncation
         physics.run_simulation(500)
         self.assertPoint((5, 0), physics.velocity)
 
     def test_simulate_negative_x(self):
         """Negative x acceleration decreases velocity."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(-2, 0),
-                            gravity=(-3, 0), terminal_velocity=(100, 100))
+        physics = Physics2d(mass=1, gravity=(-3, 0), terminal_velocity=(5, 5))
+        physics.acceleration.set((-2, 0))
         physics.run_simulation(500)
-        self.assertPoint((-3, 0), physics.velocity)  # -3 from int rounding
+        self.assertPoint((-2, 0), physics.velocity)  # -2 from int truncation
         physics.run_simulation(500)
         self.assertPoint((-5, 0), physics.velocity)
 
     def test_simulate_positive_y(self):
         """Positive y acceleration increases velocity."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(0, 2),
-                            gravity=(0, 3), terminal_velocity=(100, 100))
+        physics = Physics2d(mass=1, gravity=(0, 3), terminal_velocity=(5, 5))
+        physics.acceleration.set((0, 2))
+
         physics.run_simulation(500)
-        self.assertPoint((0, 2), physics.velocity)  # 2 from int rounding
+        self.assertPoint((0, 2), physics.velocity)  # 2 from int truncation
         physics.run_simulation(500)
         self.assertPoint((0, 5), physics.velocity)
 
     def test_simulate_negative_y(self):
         """Negative y acceleration decreases velocity."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(0, -2),
-                            gravity=(0, -3), terminal_velocity=(100, 100))
+        physics = Physics2d(mass=1, gravity=(0, -3), terminal_velocity=(5, 5))
+        physics.acceleration.set((0, -2))
+
         physics.run_simulation(500)
-        self.assertPoint((0, -3), physics.velocity)  # -3 from int rounding
+        self.assertPoint((0, -2), physics.velocity)  # -2 from int truncation
         physics.run_simulation(500)
         self.assertPoint((0, -5), physics.velocity)
 
@@ -102,28 +110,31 @@ class TestPhysics2d(unittest.TestCase):
 
     def test_simulate_positive_x_terminal_velocity(self):
         """Positive x velocity caps at terminal velocity without gravity."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(10, 0),
-                            gravity=(10, 0), terminal_velocity=(5, 0))
+        physics = Physics2d(mass=1, gravity=(10, 0), terminal_velocity=(5, 0))
+        physics.acceleration.set((10, 0))
+
         physics.run_simulation(1000)
         self.assertPoint((5, 0), physics.velocity)
 
     def test_simulate_negative_x_terminal_velocity(self):
         """Negative x velocity caps at terminal velocity without gravity."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(-10, 0),
-                            gravity=(-10, 0), terminal_velocity=(5, 0))
+        physics = Physics2d(mass=1, gravity=(-10, 0), terminal_velocity=(5, 0))
+        physics.acceleration.set((-10, 0))
+
         physics.run_simulation(1000)
         self.assertPoint((-5, 0), physics.velocity)
 
     def test_simulate_positive_y_terminal_velocity(self):
         """Positive y velocity caps at terminal velocity without gravity."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(0, 10),
-                            gravity=(0, 10), terminal_velocity=(0, 5))
+        physics = Physics2d(mass=1, gravity=(0, 10), terminal_velocity=(0, 5))
+        physics.acceleration.set((0, 10))
+
         physics.run_simulation(1000)
         self.assertPoint((0, 5), physics.velocity)
 
     def test_simulate_negative_y_terminal_velocity(self):
         """Negative y velocity caps at terminal velocity without gravity."""
-        physics = Physics2d(mass=1, velocity=(0, 0), acceleration=(0, -10),
-                            gravity=(0, -10), terminal_velocity=(0, 5))
+        physics = Physics2d(mass=1, gravity=(0, -10), terminal_velocity=(0, 5))
+        physics.acceleration.set((0, -10))
         physics.run_simulation(1000)
         self.assertPoint((0, -5), physics.velocity)
