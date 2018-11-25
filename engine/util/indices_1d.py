719c376ec9acb06a878e871580ea44621818c49e
Kate Hart <codehearts@users.noreply.github.com>
2018-11-25 00:59:51 -0800

    Add utility to horizontally flip 1d matrix indices

--- engine/util/indices_1d.py
+++ engine/util/indices_1d.py
@@ -34,3 +34,41 @@ def flip_1d_index_vertically(index, rows, columns):
     offset_column = index % columns
 
     return offset_row + offset_column
+
+
+def flip_1d_index_horizontally(index, rows, columns):
+    """Finds the index to the corresponding horizontal-flipped 1d matrix value.
+
+    Consider a 1d matrix [1, 2, 3, 4, 5, 6] with 3 rows and 2 columns. The
+    original and horizontally flipped representations are shown below.
+
+        1 2    2 1
+        3 4 -> 4 3
+        5 6    6 5
+
+    This function allows the translated matrix to be accessed using indices
+    into the original matrix, such that index 0 (value 4) becomes index 1
+    (value 2, corresponding to index 0 of the flipped matrix).
+
+    Args:
+        index (int): Index into the original 1d matrix.
+        rows (int): Number of rows in the matrix.
+        columns (int): Number of columns in the matrix.
+
+    Returns:
+        The index for the corresponding value of the horizontally flipped
+        matrix, as an int.
+    """
+    # Get current row in 1d matrix, from 0 to rows-1
+    current_row = index // columns
+
+    # Get current column in 1d matrix, from 0 to columns-1
+    current_column = index % columns
+
+    # Flip column (0 -> columns-1, ..., columns-1 -> 0, etc.)
+    flipped_column = columns - current_column - 1
+
+    # Calculate total number of entries on preceding rows
+    offset_row = current_row * columns
+
+    return offset_row + flipped_column
--- engine/util/tests/test_indices_1d.py
+++ engine/util/tests/test_indices_1d.py
@@ -1,3 +1,4 @@
+from ..indices_1d import flip_1d_index_horizontally
 from ..indices_1d import flip_1d_index_vertically
 import unittest
 
@@ -22,6 +23,28 @@ class Test1dIndicesUtils(unittest.TestCase):
 
         def convert(i):
             return original_list[flip_1d_index_vertically(i, rows, columns)]
+
+        actual_list = [convert(index) for index in range(rows * columns)]
+
+        self.assertEqual(expected_list, actual_list)
+
+    def test_flip_1d_index_horizontally(self):
+        """Gives index for corresponding value in horizontal-flipped matrix."""
+        rows = 3
+        columns = 4
+
+        original_list = [
+            1, 2, 3, 4,
+            5, 6, 7, 8,
+            9, 10, 11, 12]
+
+        expected_list = [
+            4, 3, 2, 1,
+            8, 7, 6, 5,
+            12, 11, 10, 9]
+
+        def convert(i):
+            return original_list[flip_1d_index_horizontally(i, rows, columns)]
 
         actual_list = [convert(index) for index in range(rows * columns)]
 
