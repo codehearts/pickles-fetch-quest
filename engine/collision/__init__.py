76d95fc788f2ff5feca8c0c667ccca36f243d9a4
Kate Hart <codehearts@users.noreply.github.com>
2020-09-04 23:08:40 -0700

    Remove old collision resolver in favor of World2d

--- engine/collision/__init__.py
+++ engine/collision/__init__.py
@@ -1,14 +1,9 @@
 from .collision_cache import CollisionCache
 from .collision_resolution_game_object import resolve_game_object_collision
-from .collision_resolver_2d import CollisionResolver2d
-from .collision_resolver_entry import DETECT_COLLISIONS, RESOLVE_COLLISIONS
 from .positional_collision_cache import PositionalCollisionCache
 
 __all__ = [
     'resolve_game_object_collision',
     'CollisionCache',
-    'CollisionResolver2d',
     'PositionalCollisionCache',
-    'DETECT_COLLISIONS',
-    'RESOLVE_COLLISIONS',
 ]
